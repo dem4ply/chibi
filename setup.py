ae5cf70edcdcaa03134edab1bde9db8eb07448b8
dem4ply <dem4ply@gmail.com>
2019-02-11 12:49:30 -0600

    remove circuits from chibi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
 
 setup(
     name='chibi',
-    version='1.0',
+    version='0.4',
     description='',
     # long_description=README,
     license='',
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'GitPython>=2.1.5', 'requests>=2.19.1', 'pika>=0.12.0', 'fleep==1.0.1',
-        'Pillow==5.3.0', 'Adafruit-MCP3008==1.0.2', 'pytaglib==1.4.4'
+        'Pillow==5.3.0', 'pytaglib==1.4.4'
     ],
     dependency_links = [],
     classifiers=[
