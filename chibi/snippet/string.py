e8c37f67e06db66dd3be8ec4517187a96ecdf700
dem4ply <dem4ply@gmail.com>
2019-02-26 05:50:43 -0600

    add more strings

--- chibi/snippet/string.py
+++ chibi/snippet/string.py
@@ -1,6 +1,9 @@
-from .regex import separate_camelcase_phase_1, separate_camelcase_phase_2
-from ._string import codepoint, replacement
+import _string
+import re
 from collections import defaultdict
+
+from .extra_string import codepoint, replacement
+from .regex import separate_camelcase_phase_1, separate_camelcase_phase_2
 
 
 __all__ = [ 'camel_to_snake' ]
@@ -24,3 +27,61 @@ def fold_to_ascii( s ):
     default_translate_table = defaultdict( none_factory, translate_table )
 
     return s.translate( default_translate_table )
+
+
+
+def replace_with_dict( s, d ):
+    """
+    replaces the substrings in the dictionary keys with their values
+
+    Parameters
+    ----------
+    s: string
+    d: dict
+
+    Returns
+    -------
+    string
+
+    Examples
+    --------
+    >>>s1 = "asdf qwer"
+    >>>d1 = { 'asdf', 'foo' }
+    >>>d2 = { 'qwer', 'bar' }
+    >>>replace_with_dict( s1, d1)
+    "foo qwer"
+    >>>replace_with_dict( s1, d2)
+    "asdf bar"
+    """
+    if s is None:
+        return None
+    pattern = re.compile( r'\b(' + '|'.join( d.keys() ) + r')\b' )
+    return pattern.sub( lambda x: d[ x.group() ], s )
+
+
+def get_the_number_of_parameters( s ):
+    """
+    return the number of parameters have a string
+
+    Parameters
+    ----------
+    s: string
+
+    Example
+    -------
+    >>>get_the_number_of_parameters( "{}" )
+    1
+    >>>get_the_number_of_parameters( "pi-piru {}" )
+    1
+    >>>get_the_number_of_parameters( "{} pi-pipu {}" )
+    2
+    """
+    list_of_params = list( _string.formatter_parser( s ) )
+    return len( list_of_params )
+
+
+def decode( s, code='utf-8' ):
+    '''
+    decode a string
+    '''
+    return s.decode( code )
