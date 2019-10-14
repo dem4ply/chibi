acd1764d5c77421a41a49f4a73ecc1107acabef8
dem4ply <dem4ply@gmail.com>
2019-10-14 12:59:38 -0500

    add missing dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = open(os.path.join( here, 'README.rst' ) ).read()
 
 setup(
     name='chibi',
-    version='0.5.3',
+    version='0.5.4',
     description='',
     long_description=README,
     license='',
@@ -18,7 +18,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         'GitPython>=2.1.5', 'requests>=2.19.1', 'pika>=0.12.0', 'fleep>=1.0.1',
-        'Pillow>=5.3.0', 'dateutils>=0.6.6', 'xmltodict>=0.12.0'
+        'Pillow>=5.3.0', 'dateutils>=0.6.6', 'xmltodict>=0.12.0',
+        'pyyaml>=5.1.2'
     ],
     dependency_links = [],
     url='https://github.com/dem4ply/chibi',
