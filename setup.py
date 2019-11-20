f4b91b06f60e47d60f4fc308eb412709d688a1e5
dem4ply <dem4ply@gmail.com>
2019-11-20 12:19:31 -0600

    Bump version: 0.6.0 → 0.6.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = open(os.path.join( here, 'README.rst' ) ).read()
 
 setup(
     name='chibi',
-    version='0.6.0',
+    version='0.6.1',
     description='',
     long_description=README,
     license='',
