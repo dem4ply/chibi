b051ad13e7115a3ebdf44c1150697edaa2d02ce5
dem4ply <dem4ply@gmail.com>
2020-02-21 20:56:24 -0600

    Bump version: 0.7.1 → 0.7.2

--- chibi/__init__.py
+++ chibi/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Dem4ply"""
 __email__ = 'dem4ply@gmail.com'
-__version__ = '0.7.1'
+__version__ = '0.7.2'
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = open(os.path.join( here, 'README.rst' ) ).read()
 
 setup(
     name='chibi',
-    version='0.7.1',
+    version='0.7.2',
     description='',
     long_description=README,
     license='',
