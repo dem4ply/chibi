15dcab97e5a10e460db2942228607e5571b12a34
dem4ply <dem4ply@gmail.com>
2020-02-23 16:37:12 -0600

    Bump version: 0.7.2 → 0.7.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 setup(
     name='chibi',
     keywords='chibi',
-    version='0.7.2',
+    version='0.7.3',
     description='',
     long_description=readme + '\n\n' + history,
     license="WTFPL",
