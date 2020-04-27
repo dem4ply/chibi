216bb35aacef24d849173c9b0d6a4d3d9d961e78
dem4ply <dem4ply@gmail.com>
2020-04-27 15:43:16 -0500

    Bump version: 0.8.3 → 0.8.4

--- chibi/__init__.py
+++ chibi/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Dem4ply"""
 __email__ = 'dem4ply@gmail.com'
-__version__ = '0.8.3'
+__version__ = '0.8.4'
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 setup(
     name='chibi',
     keywords='chibi',
-    version='0.8.3',
+    version='0.8.4',
     description='',
     long_description=readme + '\n\n' + history,
     license="WTFPL",
