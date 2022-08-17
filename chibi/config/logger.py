6bd8ffb870c56c839ff6f609b56e7bebf8fa275d
dem4ply <dem4ply@gmail.com>
2022-08-17 04:56:35 -0500

    se agrega force en el basic congig para loggers

--- chibi/config/logger.py
+++ chibi/config/logger.py
@@ -4,10 +4,11 @@ import logging
 __all__ = [ 'basic_config', 'silent' ]
 
 
-def basic_config( level=logging.INFO ):
+def basic_config( level=logging.INFO, force=True ):
     from . import configuration
+    level = logging.getLevelName( level )
     logger_formarter = '%(levelname)s %(asctime)s %(name)s %(message)s'
-    logging.basicConfig( level=level, format=logger_formarter )
+    logging.basicConfig( level=level, format=logger_formarter, force=force )
     if configuration.env_vars.PYTHON_UNITTEST_LOGGER:
         level = configuration.env_vars.PYTHON_UNITTEST_LOGGER
 
