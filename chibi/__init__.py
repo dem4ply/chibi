1624ac92933a5e5f172458bbec5703a5cb665460
dem4ply <dem4ply@gmail.com>
2023-04-13 00:34:35 -0600

    se atrapa la excepcion de valueerror en el caso de que se use una version vieja a de python

--- chibi/__init__.py
+++ chibi/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 __author__ = """Dem4ply"""
 __email__ = 'dem4ply@gmail.com'
 __version__ = '0.11.8'
--- chibi/config/logger.py
+++ chibi/config/logger.py
@@ -8,7 +8,19 @@ def basic_config( level=logging.INFO, force=True ):
     from . import configuration
     level = logging.getLevelName( level )
     logger_formarter = '%(levelname)s %(asctime)s %(name)s %(message)s'
-    logging.basicConfig( level=level, format=logger_formarter, force=force )
+    try:
+        logging.basicConfig(
+            level=level, format=logger_formarter, force=force )
+    except ValueError as e:
+        if 'Unrecognised argument(s): force' == str( e ):
+            logging.basicConfig( level=level, format=logger_formarter )
+            logger = logging.getLogger( 'chibi.config.logger' )
+            logger.warning(
+                "estas usando una version vieja de python por eso no se "
+                "puede usar la el parametro 'force' para asignar el "
+                "formato del los logs" )
+        else:
+            raise
     if configuration.env_vars.PYTHON_UNITTEST_LOGGER:
         level = configuration.env_vars.PYTHON_UNITTEST_LOGGER
 
